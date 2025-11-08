03d0851803e185f0f20b4ceec89bfa65321f2878
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-11-08 14:04:59 -0500

    player basics

---
+++ pile/pile.gd
@@ -0,0 +1,4 @@
+class_name Pile
+extends Area2D
+
+@export var item_scene: PackedScene
--- player/player.gd
+++ player/player.gd
@@ -1,1 +1,32 @@
-extends Node2D
+extends CharacterBody2D
+
+const SPEED = 200.0
+const ACCEL = 0.25
+
+var held_item: Node
+
+@onready var interaction_area: Area2D = $InteractionArea
+
+
+func hold_item(item: Node) -> void:
+	drop_item()
+	held_item = item
+	add_child(item)
+
+
+func drop_item() -> void:
+	if not held_item: return
+	held_item = null
+
+
+func _physics_process(delta: float) -> void:
+	velocity.x = lerpf(velocity.x, Input.get_axis(&"ui_left", &"ui_right") * 100.0, ACCEL)
+	velocity.y = 100.0
+	move_and_slide()
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"ui_accept"):
+		for area: Area2D in interaction_area.get_overlapping_areas():
+			if area is Pile and area.item_scene:
+				hold_item(area.item_scene.instantiate())
