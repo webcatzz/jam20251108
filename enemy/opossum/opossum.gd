1e2d8b6b22e395126655559183f1fed494fe1086
Mr-Hippo2 <klemmashton@gmail.com>
2025-11-09 10:57:37 -0500

    i'm opossuming

--- enemy/opossum/opossum.gd
+++ enemy/opossum/opossum.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
-
+var opossum_speed = 35.7  
+@onready var play_animation = $AnimatedSprite2D
+var direction = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	play_animation.play("Walk_Left")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.x -= opossum_speed * delta * direction
+
+
+
+func _on_timer_timeout() -> void:
+	if direction == 1:
+		direction = -1
+		play_animation.play("Walk_Right")
+	else:
+		direction = 1
+		play_animation.play("Walk_Left")
+
+
+func _on_area_entered(area: Area2D) -> void:
+	Game.take_damage()
