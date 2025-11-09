0f04108443502e3c2bee76d4983a1fd90ea1e8e9
Mr-Hippo2 <klemmashton@gmail.com>
2025-11-08 19:49:09 -0500

    added new mouse sprite to better fit scenario

--- enemy/mouse/mouse.gd
+++ enemy/mouse/mouse.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 const SEED = preload("res://enemy/projectile/seed.tscn") 
 
+
 #decides the time and placement of the seeds spawer
 func _on_seed_spawner_timeout() -> void:
 	var new_seed = SEED.instantiate()
